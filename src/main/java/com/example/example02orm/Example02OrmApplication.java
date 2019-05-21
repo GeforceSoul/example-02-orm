2679e040256886d16bbe68c19174ad536ea8d366
a1181207784@163.com <1999418hhh>
2019-05-21 13:29:10 +0800

    实体对象状态实验
    2019-5-21

---
+++ src/main/java/com/example/example02orm/Example02OrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.example02orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example02OrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example02OrmApplication.class, args);
+    }
+
+}
